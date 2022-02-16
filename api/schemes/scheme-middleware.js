4915d99dd82eea79a2a4ea01d987d6375b4a7b0e
bugsbrog <bugsbrog@gmail.com>
2022-02-15 19:38:28 -0700

    All mw done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -21,14 +21,6 @@ const checkSchemeId = async (req, res, next) => {
     }
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = async (req, res, next) => {
   const { scheme_name } = req.body
     try {
@@ -45,17 +37,20 @@ const validateScheme = async (req, res, next) => {
     }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
-const validateStep = (req, res, next) => {
-
+const validateStep = async (req, res, next) => {
+  const { instructions, step_number } = req.body
+    try {
+      if (!instructions || typeof instructions !== 'string' || !instructions.trim() || typeof step_number !== 'number' || step_number < 1) {
+        next({
+          status: 400,
+          message: 'invalid step'
+        })
+      } else {
+          next()
+      }
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports = {
